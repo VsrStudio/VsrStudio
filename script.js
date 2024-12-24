4a50d10e5582979e649e54dd9c9e622afb8a83b4
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-12-24 11:30:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ document.querySelectorAll(".btn").forEach(button => {
         button.style.transform = "scale(1)";
     });
 });
+
+function searchPlugin() {
+    var input, filter, cards, card, title, i;
+    input = document.getElementById('searchInput');
+    filter = input.value.toLowerCase();
+    cards = document.getElementsByClassName('plugin-card');
+
+    for (i = 0; i < cards.length; i++) {
+        card = cards[i];
+        title = card.getElementsByTagName('h3')[0];
+
+        if (title.innerHTML.toLowerCase().indexOf(filter) > -1) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+        }
+    }
+}
